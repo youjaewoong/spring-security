5f86b9d5583a5ec4cef625955490ec32d6fbe7a2
DESKTOP-FIF88VS\gos10 <n_nv711@naver.com>
2022-09-19 18:18:10 +0900

    feat: 누락수정

--- src/main/java/com/springsecurity/account/AccountController.java
+++ src/main/java/com/springsecurity/account/AccountController.java
@@ -2,13 +2,27 @@ package com.springsecurity.account;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AccountController {
 
+    @Autowired
+    AccountService accountService;
+
+     // 회원가입 TEST
+    @ResponseBody
+    @GetMapping("/account/{role}/{username}/{password}")
+    public Account createAccount(@ModelAttribute Account account) {
+        return accountService.createNew(account);
+    }
+    
+        
     @GetMapping("/access-denied")
     public String createAccount(Principal principal, Model model) {
     	model.addAttribute("name", principal.getName());
